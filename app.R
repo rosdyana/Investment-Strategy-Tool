1162a9752e687332ffd8e9c8fa11460f757224da
Rosdyana Kusuma <rosdyana.kusuma@gmail.com>
2017-04-29 03:10:26 +0800

    Update app.R

--- app.R
+++ app.R
@@ -19,7 +19,8 @@ ui <- pageWithSidebar(
       end = Sys.Date(),
       format = 'yyyy-mm-dd'
     ),
-    actionButton("get", "Get Stock"),
+    actionButton("get", "Run", icon("check-circle")),
+    actionButton("about", "About", icon("info-circle")),
     hr()
   ),
   mainPanel(  tabsetPanel(
@@ -129,6 +130,19 @@ server <- function(input, output) {
   
   observeEvent(input$get, {
     mainfunc(dataInput())
+  })
+  
+  observeEvent(input$about, {
+    showModal(modalDialog(
+      title = span(tagList(icon("info-circle"), "About")),
+      tags$div(
+        HTML(
+          "<img src='https://avatars1.githubusercontent.com/u/4516635?v=3&s=460' width=150><br/><br/>",
+          "<p>Developer : Rosdyana Kusuma</br>Email : <a href=mailto:rosdyana.kusuma@gmail.com>rosdyana.kusuma@gmail.com</a></br>linkedin : <a href='https://www.linkedin.com/in/rosdyanakusuma/' target=blank>Open me</a></p>"
+        )
+      ),
+      easyClose = TRUE
+    ))
   })
 }
 
